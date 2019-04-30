8dfb69517113487badaf68f7403a52d76bc57806
CeroBits <garon.santana@gmail.com>
2019-04-30 17:29:54 -0300

    filtro de playlist y video

--- gapi.js
+++ gapi.js
@@ -35,7 +35,7 @@ try{
   }).then(function() {
     // 3. Initialize and make the API request.
     return gapi.client.request({
-      'path': 'https://www.googleapis.com/youtube/v3/search?q='+query+'&part=snippet'
+      'path': 'https://www.googleapis.com/youtube/v3/search?q='+query+'&part=snippe&type=video,playlistt'
     })
   }).then(function(response) {
     console.log(response.result);

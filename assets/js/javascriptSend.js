c4390cb4b72273fbb482234d9ca4969a629c54b1
CeroBits <garon.santana@gmail.com>
2019-05-03 14:54:55 -0300

    Update javascriptSend.js

--- assets/js/javascriptSend.js
+++ assets/js/javascriptSend.js
@@ -1,27 +1,28 @@
-function sendMail(){	
-	 var nameContacto = document.getElementById("name").value;
-	 var correoContacto = document.getElementById("email").value;
-	  var bodyCorreo = document.getElementById("message").value;
-	  
-jQuery.ajax({
-  type: “POST”,
-  url: “https://mandrillapp.com/api/1.0/messages/send.json”,
-  data: {
-    ‘key’: ‘eb1bbc0d6b53947904f738891d6e7013-us20’,
-    ‘message’: {
-      ‘from_email’: correoContacto,
-      ‘to’: [
-          {
-            ‘email’: ‘garon.santana@gmail.com’,            
-            ‘type’: ‘to’
-          },
-        ],
-      ‘autotext’: ‘true’,
-      ‘subject’: bodyCorreo,
-      ‘html’: ‘YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!’
-    }
-  }
- }).done(function(response) {
-   console.log(response); 
- });
+function sendMail() {
+		var nameContacto = document.getElementById("name").value;
+		var correoContacto = document.getElementById("email").value;
+		var bodyCorreo = document.getElementById("message").value;
+
+    $.ajax({
+      type: "POST",
+      url: "https://mandrillapp.com/api/1.0/messages/send.json",
+      data: {
+        "key": "eb1bbc0d6b53947904f738891d6e7013-us20",
+        "message": {
+          "from_email": correoContacto,
+          "to": [
+              {
+                "email": "garon.santana@gmail.com",
+                "name": "RECIPIENT NAME (OPTIONAL)",
+                "type": "to"
+              }
+            ],
+          "autotext": "true",
+          "subject": bodyCorreo,
+          "html": "YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!"
+        }
+      }
+     }).done(function(response) {
+       console.log(response); // if you"re into that sorta thing
+     });
 }

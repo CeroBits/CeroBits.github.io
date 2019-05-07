f9ba71daccecf4a052915bca69d88c32c7ded093
CeroBits <garon.santana@gmail.com>
2019-05-07 11:06:07 -0300

    Update spotyhear.js

--- spotyhear.js
+++ spotyhear.js
@@ -1,3 +1,21 @@
-  var urlParams = new URLSearchParams(window.location.search);
-  var token = urlParams.get('code');
-  console.log("bearer token: "+token);
+try{
+var urlParams = new URLSearchParams(window.location.search);
+var authorizationBasic = urlParams.get('code');
+var uri = "https://api.spotify.com/v1/search?type=track&q=";
+var str = "bach";
+var searchParam = encodeURIComponent(str);
+var request = new XMLHttpRequest();
+		request.open('GET', uri+searchParam, true);
+		request.withCredentials = true;
+		request.setRequestHeader('Authorization', 'Bearer ' +
+		authorizationBasic);
+		request.send();
+		request.onreadystatechange = function () {
+		if (request.readyState === 4) {
+		alert(request.responseText);
+		}
+	};
+}
+catch(error){
+alert(error.message);
+}
